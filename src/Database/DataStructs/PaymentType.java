661698304e0fdfd6d3f6dfd0ade195b075831ba9
Iain <iainlim33cc@gmail.com>
2024-04-24 16:55:01 +0800

    Added getType to PaymentType enum

--- src/Database/DataStructs/PaymentType.java
+++ src/Database/DataStructs/PaymentType.java
@@ -7,6 +7,21 @@ public enum PaymentType {
         return toChar;
     }
 
+    public String getType() {
+        switch (this) {
+            case CREDIT_CARD:
+                return "Credit Card";
+            case DEBIT_CARD:
+                return "Credit Card";
+            case QR:
+                return "Credit Card";
+            case OTHERS:
+                return "Credit Card";
+            default:
+                return null;
+        }
+    }
+
     private final char toChar;
     PaymentType(char toChar) {
         this.toChar = toChar;
